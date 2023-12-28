770142b8939b3ee7f2338061374adb9c010a485b
Aurélien Leclercq <aurelien.leclercq@epitech.eu>
2023-12-28 23:56:26 +0100

    feat: player animated while moving

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,12 +6,12 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed;
     public Rigidbody2D rb2d;
     private Vector2 moveInput;
+    private Animator anim;
+
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
-
-    // Update is called once per frame
     void Update()
     {
         moveInput.x = Input.GetAxisRaw("Horizontal");
@@ -20,5 +20,26 @@ public class PlayerMovement : MonoBehaviour
         moveInput.Normalize();
 
         rb2d.velocity = moveInput * moveSpeed;
+        bool isWalking = false;
+
+        if (moveInput != Vector2.zero)
+        {
+            anim.SetBool("isWalking", true);
+            anim.SetFloat("X", moveInput.x);
+            anim.SetFloat("Y", moveInput.y);
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+        }
+
+        if (moveInput.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (moveInput.x > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
     }
 }
