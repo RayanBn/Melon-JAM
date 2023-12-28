f7951cd82697b34b64595b7c76f1c56100d15b80
RayanBn <rayan.banni@epitech.eu>
2023-12-28 19:49:59 +0100

    feat: LightFlicker

--- Assets/Scripts/LightFlicker.cs
+++ Assets/Scripts/LightFlicker.cs
@@ -5,14 +5,11 @@ using UnityEngine.Rendering.Universal;
 
 public class LightFlicker : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public Light2D light;
-    void Start()
-    {
-    }
+    public new Light2D light;
 
-    // Update is called once per frame
     void Update()
     {
+        light.pointLightInnerRadius = Mathf.Lerp(light.pointLightInnerRadius, Random.Range(1, 2.7f), Time.deltaTime * 2f);
+        light.pointLightOuterRadius = Mathf.Lerp(light.pointLightOuterRadius, Random.Range(3, 3.7f), Time.deltaTime * 2f);
     }
 }
